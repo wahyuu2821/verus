20805da275d7d8fd086b22a2b33f085329285b20
wahyuu2821 <63901359+wahyuu2821@users.noreply.github.com>
2021-10-16 16:26:15 +0700

    Create start_test.sh

---
+++ start_test.sh
@@ -0,0 +1,11 @@
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://ap.luckpool.net:395$
